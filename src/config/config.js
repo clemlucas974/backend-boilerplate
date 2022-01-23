4f498d3613f634ad710ff392430c4b466833d9ec
Clément Lucas <clement.lucas@quitoque.fr>
2022-01-23 14:35:16 +0100

    fix(sequelize): config connection pool

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,12 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
   test: {
     username: process.env.DB_USER,
@@ -14,6 +20,12 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
   production: {
     username: process.env.DB_USER,
@@ -21,5 +33,11 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
 };
